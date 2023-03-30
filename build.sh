7bda979a9a0c33f03b032bad7bd464626510bfdb
dbayure <dbayure@gmail.com>
2023-03-30 14:29:55 -0300

    correccion build

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pippip install --force-reinstall -U setuptools
-requirements.txt
+pip install --upgrade pippip install --force-reinstall -U setuptools requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
