3080797936c0ab834bcb789c67eac63e7a44a1da
dbayure <dbayure@gmail.com>
2023-03-30 14:37:52 -0300

    correccion build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install --upgrade pip 
 pip install --force-reinstall -U setuptools 
-pip install requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
